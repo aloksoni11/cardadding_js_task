bbd9959ebea4830feeec09cadef8ae00c3500668
aloksoni11 <sonialok5777@gmail.com>
2019-05-01 21:16:33 +0530

    last init

--- js/main.js
+++ js/main.js
@@ -1,25 +1,48 @@
-function update() {
-    var x = window.getComputedStyle(document.getElementById("update")).display;
+function update(t) {
+
+    var item = document.getElementById("update" + t);
+
+    var x = window.getComputedStyle(item).display;
+
+    console.log(x);
+
     if (x != "none") {
-        var a = document.getElementById("update");
-        a.style.display = "none";
+        item.style.display = "none";
+    } else {
+        item.style.display = "block";
     }
-    else {
-        var a = document.getElementById("update");
-        a.style.display = "inline-block";
-    }
+
+
+
 }
 
+var count = 0;
+
 function addCard() {
+
+    count++;
+
     var add = document.getElementById("AddCard");
-    add.insertAdjacentHTML('beforeEnd', "<div class='col s12 l4'><div class='card' id='card'><div class='card-image' id='card-img'> <span class='right  menu-btn'></span> <span class='right menu-btn-icon'><i class='material-icons ' onclick='update()'>more_vert</i></span><div class='' id='update'><ul class='collection'><li class='collection-item' onclick='UpdateBtn()'>UPDATE</li><li class='collection-item'>DELETE</li></ul></div> <img src='images/5.jpg' alt='' class='responsive-img materialboxed'> </div><div class='card-content'><h1 class='card-title text-justify' contentEditable='false' Id='titleNm'>Rabbit</h1><p contentEditable='flase' Id='details'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsum maiores minima culpa, lorem</p></div></div></div>");
+    add.insertAdjacentHTML('beforeEnd', "<div class='col s12 l4'><div class='card' id='card'><div class='card-image' id='card-img'> <span class='right  menu-btn'></span> <span class='right menu-btn-icon'><i class='material-icons ' onclick='update(" + count + ")'>more_vert</i></span><div class='Update' id=update" + count + "><ul class='collection'><li class='collection-item' onclick='UpdateBtn()'>UPDATE</li><li class='collection-item' onclick='DeleteBtn(this)'>DELETE</li></ul></div> <img src='images/5.jpg' alt='' class='responsive-img materialboxed'> </div><div class='card-content'><h1 class='card-title text-justify' id='titleNm'>Rabbit</h1><p id='details'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsum maiores minima culpa, lorem</p></div></div></div>");
     $('.materialboxed').materialbox();
 }
 
 function UpdateBtn() {
-    var x = document.getElementById("titleNm").style.contentEditable;
-    console.log(x);
+    var x = document.getElementById("titleNm");
     var y = document.getElementById("details");
-    x.style.contentEditable = "true";
-    y.style.contentEditable = "true";
+    console.log(x);
+    if (x.contentEditable != "true" && y.contentEditable != "true") {
+        x.contentEditable = "true";
+        y.contentEditable = "true";
+    } else {
+        x.contentEditable = "false";
+        y.contentEditable = "false";
+    }
+
+}
+
+function DeleteBtn(t) {
+
+    var x = t.parentNode.parentNode.parentNode.parentNode.parentNode;
+    x.remove();
 }
